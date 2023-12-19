d319c84fe30039772ba7a8931be8541dd62cdba8
lw86ruwo <Leon@wenderoth.de>
2023-12-19 20:20:49 +0100

    reformat

--- flask_app/files.py
+++ flask_app/files.py
@@ -8,30 +8,28 @@ main_routes = Blueprint('main_routes', __name__, url_prefix='/data')
 
 @main_routes.route('/upload', methods=['POST'])
 def upload_files():
-		files = request.files.getlist('file')
-		form = request.form
+	files = request.files.getlist('file')
+	form = request.form
 
-		authorization = request.headers.get("authorization")
-		organisation_id = int(form.get("organisationId"))
+	authorization = request.headers.get("authorization")
+	organisation_id = int(form.get("organisationId"))
 
-		token = tokenDecode(authorization)
+	token = tokenDecode(authorization)
 
-		document_ids: list = []
+	document_ids: list = []
 
-		for file in files:
-			content_type = file.content_type
-			if 'text/plain' in content_type:
-				filename = file.filename
-				content = str(file.stream.read().decode('utf-8'))
-				dokument_id = addDocument(filename, content, organisation_id, token.id)
-				document_ids.append(dokument_id)
-			else:
-				document_ids.append(f"wrong type {content_type}")
+	for file in files:
+		content_type = file.content_type
+		if 'text/plain' in content_type:
+			filename = file.filename
+			content = str(file.stream.read().decode('utf-8'))
+			dokument_id = addDocument(filename, content, organisation_id, token.id)
+			document_ids.append(dokument_id)
+		else:
+			document_ids.append(f"wrong type {content_type}")
 
-		if all(isinstance(document_ids, str) for _ in document_ids):
-			return make_response(document_ids, 400)
-		if any(isinstance(document_ids, str) for _ in document_ids):
-			return make_response(document_ids, 207)
-		return make_response(document_ids, 201)
-
-
+	if all(isinstance(document_ids, str) for _ in document_ids):
+		return make_response(document_ids, 400)
+	if any(isinstance(document_ids, str) for _ in document_ids):
+		return make_response(document_ids, 207)
+	return make_response(document_ids, 201)
